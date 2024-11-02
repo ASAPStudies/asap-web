17897c0f525684554953911eff2ed16007c3956b
yelpoea <yelpoeayahaya@gmail.com>
2024-11-02 14:18:30 +0000

    Done with contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IStaticMethods } from 'preline/preline';
 import { Router, Event, NavigationEnd } from '@angular/router';
+
 declare global {
   interface Window {
     HSStaticMethods: IStaticMethods;
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormService } from '../../services/data/form.service';
+import { from } from 'rxjs';
+import { ToastService, AngularToastifyModule } from 'angular-toastify'; 
 @Component({
   selector: 'app-contact',
   standalone: true,
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, AngularToastifyModule],
   
 })
 export class ContactComponent {
@@ -19,7 +27,49 @@ export class ContactComponent {
     message: new FormControl(''),
   });
 
-  submit(){
-    console.log(this.formData.value)
+  constructor(private toast:ToastService) {}
+  submit() {
+    // Check if the form is valid
+    if (this.formData.invalid) {
+            this.openToast('info', 'Incomplete Form');
+
+      return;
+    }
+
+    const dataSub = from(
+      fetch('https://formsubmit.co/ajax/yelpoeayahaya@gmail.com', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(this.formData.value), // Use form value here
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then((data) => console.log(data))
+        .catch((error) =>
+          this.toast.error("There was and error")
+        )
+    );
+    if (this.formData.valid){
+      this.openToast("info", "Complete Form")
+    }
+    dataSub.subscribe(()=>{
+      this.openToast("success", "Message sent")
+      this.formData.reset()
+    });
+    
+  }
+
+  openToast(type:string, message:string){
+    type === 'success'
+      ? this.toast.success(message)
+      : this.toast.info(message);
+    
   }
 }
