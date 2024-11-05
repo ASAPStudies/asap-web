c2cc15444a8ea1653c7b4e37e7f2e54538bd55c7
yelpoea <yelpoeayahaya@gmail.com>
2024-11-05 11:48:02 +0000

    done with addpage

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { NgClass } from '@angular/common';
 import { Component} from "@angular/core";
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { PromoHeaderComponent } from '../promo-header/promo-header.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgClass],
+  imports: [RouterLink, RouterLinkActive, NgClass, PromoHeaderComponent],
 })
 export class HeaderComponent {
   showSidebar: boolean = false;
---
+++ src/app/components/promo-header/promo-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-promo-header',
+    standalone:true,
+    templateUrl:'./promo-header.html'
+})
+
+export class PromoHeaderComponent {
+
+}
