e6bdb9d4cc6322c6196f02f3550c1b2274c4f015
yelpoea <yelpoeayahaya@gmail.com>
2024-11-06 07:42:37 +0000

    done with the special offer header

--- src/app/components/promo-header/promo-header.component.ts
+++ src/app/components/promo-header/promo-header.component.ts
@@ -1,9 +1,11 @@
+import { NgOptimizedImage } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
     selector:'app-promo-header',
     standalone:true,
-    templateUrl:'./promo-header.html'
+    templateUrl:'./promo-header.html',
+    imports:[NgOptimizedImage]
 })
 
 export class PromoHeaderComponent {
